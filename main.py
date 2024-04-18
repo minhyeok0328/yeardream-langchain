28e5012cf18880b01cb143c889e3734746d256cb
minhyeok0328 <alsgur7408@gmail.com>
2024-04-18 04:30:58 +0000

    chore: main.py에서 환경변수로 API_KEY 등록하게 수정 편의상 이게 맞는 것 같습니다

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
 from chatting import excute_chatting
+from core.config import API_KEY
+
+def main():
+    if 'GOOGLE_API_KEY' not in os.environ:
+        os.environ['GOOGLE_API_KEY'] = API_KEY['GEMINI']
+    
+    excute_chatting()
 
 if __name__ == '__main__':
-    excute_chatting()
+    main()
