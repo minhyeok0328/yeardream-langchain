6a7cc8c90acef9eeafafcd8abc131e20e581a2de
Jay-G <yjysl88@gmail.com>
2024-04-18 08:59:05 +0000

    split text, return 추가

--- gemini-test/crawler.py
+++ gemini-test/crawler.py
@@ -1,8 +1,8 @@
 import PyPDF2
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pdfminer.high_level import extract_text
 
-# PDF 경로
-pdf_file_path = 'Chain-of-Thought-prompting.pdf'
+pdf_file_path = 'Chain-of-Thought-prompting.pdf'    # PDF 경로 ex) './abc.pdf'
 
 class read_pdf:
     def __init__(self, pdf_file_path):
@@ -15,17 +15,17 @@ class read_pdf:
             num_pages = reader.numPages            
             # 텍스트를 추출, 리스트 저장
             text_list = []
+            splits = []
             for page_number in range(num_pages):
                 page = reader.getPage(page_number)
                 text = page.extractText()
                 text_list.append(text)
             
-            '''
-            # 텍스트 출력 또는 원하는 정보 크롤링
-            for text in text_list:
-                # 예시: 특정 텍스트가 있는 부분을 찾아 출력
-                if 'important information' in text:
-                    print(text)'''
+            splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=0)
+            splits = splitter.split_documents(text_list)
+
+        #return ' '.join(text_list)
+        return text_list, splits
 
 read_pdf.pdf_read(pdf_file_path)
 
