8b79e3e842a3330e6bd7ec2f3650d74a2930ee5a
minhyeok0328 <alsgur7408@gmail.com>
2024-04-19 03:56:47 +0000

    Revert "chore: store 추상 클래스 삭제"
    This reverts commit 9ff4da4257df8b45102ba3b704accdc5233d283f.

--- core/abstract/store.py
+++ core/abstract/store.py
@@ -2,10 +2,7 @@ from abc import ABC, abstractmethod
 from typing import List
 from langchain_core.documents.base import Document
 
-class Store(ABC):
-    def __init__(self, document: List[Document]):
-        pass
-
+class Store:
     @abstractmethod
     def add_document(self, new_document: List[Document]):
         pass
