9ff4da4257df8b45102ba3b704accdc5233d283f
minhyeok0328 <alsgur7408@gmail.com>
2024-04-19 03:55:43 +0000

    chore: store 추상 클래스 삭제
    다른 분이 만들 예정

--- core/abstract/store.py
+++ core/abstract/store.py
@@ -2,7 +2,10 @@ from abc import ABC, abstractmethod
 from typing import List
 from langchain_core.documents.base import Document
 
-class Store:
+class Store(ABC):
+    def __init__(self, document: List[Document]):
+        pass
+
     @abstractmethod
     def add_document(self, new_document: List[Document]):
         pass
